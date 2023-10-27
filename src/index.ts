ee7b0ea620c55bc0f333bf0cc64df9f0a0470c89
Guttorf <147665538+Guttorf@users.noreply.github.com>
2023-10-27 00:37:38 +0000

    type script constructor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let Cat = {
+/*let Cat = {
   nome:"Madame",
   idade: 2,
   peso: 3,
@@ -6,4 +6,30 @@ let Cat = {
     console.log(this.nome + " meow")
   }
 }
-Cat.falar()
+Cat.falar()*/
+
+/*Classe BankAccount
+Possui nome, id, senha e saldo
+Nome, id senha são fornecidos na criação
+Saldo sempre começa em 0*/
+
+
+
+class Bank  {
+  nome: string
+  saldo: number
+  id: string
+  senha: number
+  constructor(nomeFornecido: string, saldoFornecido: number, idFornecido: string, senhaFornecido: number)
+   {
+  this.nome = nomeFornecido
+  this.saldo = saldoFornecido = 1
+  this.id = idFornecido
+  this.senha = senhaFornecido
+  }
+  bankvalor () {
+    console.log(this.saldo)
+   }
+} 
+const bank = new Bank("Banerj ", 1000)
+     
